abc7e05da0303e1edd98b2d156b1bc73cebc4f91
nqghuy <kehi4705@gmail.com>
2024-01-04 21:13:21 +0700

    upload shortest path code

--- DEMO_BFS.cpp
+++ DEMO_BFS.cpp
@@ -1,62 +1,73 @@
 #include <bits/stdc++.h>
 using namespace std;
+// n is the numbers of vertices, m is the number of edges
+int n, m;
 
-void BFS(vector<int> adj[], int visited[], int vertex){
-    queue <int> outVertex;
-    outVertex.push(vertex);
-    visited[vertex] = 1;
-    while (!outVertex.empty()){
-        int currentVer = outVertex.front();
-        outVertex.pop();
-        //cout << currentVer << " ";
-        for (int x : adj[currentVer]){
+//adjacency list
+vector <int> adj[20];
+
+//mark if this vertex is visited or not
+int visited[20] = {};
+
+int parent[20] = {};
+
+void BFS(int u){
+    visited[u] = 1;
+    queue <int> q;
+    q.push(u);
+    while(!q.empty()){
+        int v = q.front();
+        q.pop();
+        for (int x : adj[v]){
             if (!visited[x]){
-                outVertex.push(x);
                 visited[x] = 1;
-            }
+                q.push(x);
+                parent[x] = v;
+            }   
         }
     }
 }
 
-
-void DFS(vector <int> adj[], int visited[], int vertex){
-    visited[vertex] = 1;
-    cout << vertex << " ";
-    for (int x : adj[vertex]){
-        if (!visited[x]){
-            DFS(adj, visited, x);
-        }
-    }
-}
-
-int connectedComponent(vector <int> adj[], int visited[], int n){
-    int cnt = 0;
-    memset(visited, 0, sizeof(visited));
-    for (int i = 1; i <= n; i++){
+void DFS(int x){
+    visited[x] = 1;
+    for (int i : adj[x]){
         if (!visited[i]){
-            cnt++;
-            DFS(adj, visited, i);
-            cout << endl;
+            parent[i] = x;
+            DFS(i);
         }
     }
-    return cnt;
+
+}
+
+void path(int x, int y){
+    vector <int> v;
+    BFS(x);
+    parent[x] = -1;
+    if (visited[y] == 0){
+        cout << "no path\n";
+        return;
+    }
+    while (y != -1){
+        v.push_back(y);
+        y = parent[y];
+    }
+    for (int i = v.size() - 1; i >= 0; i--){
+        cout << v[i] << " ";
+    }
 }
 
 int main(){
-    //n is the number of vertices
-    //m is the number of edge
-    int n, m;
     cin >> n >> m;
-    //adjacency list
-    vector <int> adj[20];
     for (int i = 0; i < m; i++){
         int startVer, endVer;
         cin >> startVer >> endVer;
+        //undirected graph
         adj[startVer].push_back(endVer);
-
-        //if not directed graph
         adj[endVer].push_back(startVer);
     }
-    int visited[20] = {};
-    cout << connectedComponent(adj, visited, n);
+
+    //s is start vertex, e is end vertex
+    int s, e;
+    cin >> s >> e;
+    path (s, e);
 }
