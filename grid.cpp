0cde2550f35627383c702b1cf3f01136b2b9143c
nqghuy <kehi4705@gmail.com>
2024-01-05 09:21:15 +0700

    show direction of shortest path code

--- grid.cpp
+++ grid.cpp
@@ -2,60 +2,101 @@
 using namespace std;
 
 int row, col;
-int grid[1001][1001] = {};
+char grid[1001][1001] = {};
 int dx[4] = {-1, 0, 0, 1};
 int dj[4] = {0, -1, 1, 0};
 
-void DFS(int i, int j){
-    grid[i][j] = 0;
-    for (int k = 0; k < 4; k++){
-        int newi = i + dx[k];
-        int newj = j + dj[k];
-        if (newi >= 1 && newi <= row && newj >= 1 && newj <= col && grid[newi][newj])   DFS(newi, newj);
-    }
-}
+map <pair<int, int>, char> mp;
+map <pair <int, int>, pair <int, int>> parent;
 
-void BFS(int i, int j){
-    grid[i][j] = 0;
+
+bool BFS(int i1, int j1, int i2, int j2){
+    //mark [i,j] is visited
+    grid[i1][j1] = 'x';
+
+    //queue for visited vertex
     queue <pair<int, int>> q;
-    q.push({i, j});
+
+    q.push({i1, j1});
     while (!q.empty()){
         pair <int, int> v = q.front();
-        cout << v.first << " " << v.second << endl;
         q.pop();
         for (int k = 0; k < 4; k++){
+            //new cordinate
             int newi = v.first + dx[k];
             int newj = v.second + dj[k];
-            if (newi >= 1 && newi <= row && newj >= 1 && newj <= col && grid[newi][newj]){
-                grid[newi][newj] = 0;
+            
+            if (newi >= 1 && newi <= row && newj >= 1 && newj <= col && grid[newi][newj] != 'x'){//if satisfy the condition
+                //v is parent of [newi, newj]
+                parent[{newi, newj}] = {v.first, v.second};
+
+                //save the direction
+                if (newi == v.first){
+                    if (newj < v.second){
+                        mp[{newi, newj}] = 'L';
+                    }
+                    else mp[{newi, newj}] = 'R';
+                }
+                else if(newi < v.first){
+                    mp[{newi, newj}] = 'U';
+                }
+                else mp[{newi, newj}] = 'D';
+
+                //mark [newi, newj] is visited
+                grid[newi][newj] = 'x';
+
+                //if B is found
+                if (newi == i2 && newj == j2){
+                    return true;
+                }
+                
+                //push visited [newi, newj] into queue
                 q.push({newi, newj});
             }
         }
-
     }
+    return false;
 }
 
-int connectedComponent(){
-    int cnt = 0;
-    for (int i = 1; i <= row; i++){
-        for (int j = 1; j <= col; j++){
-            if (grid[i][j]){
-                cnt++;
-                cout << "thanh phan lien thong thu " << cnt << ":\n";
-                BFS(i, j);
-            }
-        }
+void path(int i1, int j1, int i2, int j2){
+    if (!BFS(i1, j1, i2, j2)){
+        cout << "no path\n";
+        return;
     }
-    return cnt;
-}
+    //start point
+    mp[{i1, j1}] = 'S';
+    parent[{i1, j1}] = {-1, -1};
 
+    //contain path
+    vector <char> path;
+    while (i2 != -1 && j2 != -1){
+        path.push_back(mp[{i2, j2}]);
+        int tmp = i2;
+        i2 = parent[{i2, j2}].first;
+        j2 = parent[{tmp, j2}].second;
+    }
+
+    for (int i = path.size() - 1; i >= 0; i--){
+        cout << path[i] << " ";
+    }
+}
 
 int main(){
     cin >> row >> col;
+    int i1, j1, i2, j2;
+    memset(grid, 'x', sizeof(grid));
     for (int i = 1; i <= row; i++){
         for (int j = 1; j <= col; j++){
             cin >> grid[i][j];
+            if (grid[i][j] == 'A') {
+                i1 = i;
+                j1 = j;
+            }
+            if (grid[i][j] == 'B'){
+                i2 = i;
+                j2 = j;
+            }
         }
     }
-    cout << connectedComponent();
+    path(i1, j1, i2, j2);
 }
